2db0ffe13528c2663273947a8e2669762298e38d
SaZOsadam <soladiran25@gmail.com>
2024-03-20 13:35:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let fullName = "Sarah Oladiran";
+let age = 27;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
